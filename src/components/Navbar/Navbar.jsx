1064f2d832a48488500b19aaef19fd9257772348
Irene Riportella <riportellairene@gmail.com>
2025-05-26 23:12:12 +0200

    feat:✨ update Header component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { List, X } from '@phosphor-icons/react';
+import Logo from '../Logo/Logo';
 
 function Navbar() {
-  return <div></div>;
+  return (
+    <>
+      <div className='flex items-center'>
+        <Logo />
+        <ul className='flex pt-1.5 gap-6 ml-20'>
+          <li className='hover:font-medium'>For You</li>
+          <li className='hover:font-medium'>For Business</li>
+          <li className='hover:font-medium'>For Providers</li>
+          <li className='hover:font-medium'>Our Plans</li>
+          <li className='hover:font-medium'>Resources</li>
+          <li className='hover:font-medium'>About</li>
+        </ul>
+      </div>
+
+      {/* <List
+        size={32}
+        weight="bold"
+      />
+      <X
+        size={32}
+        weight="bold"
+      /> */}
+    </>
+  );
 }
 
 export default Navbar;
