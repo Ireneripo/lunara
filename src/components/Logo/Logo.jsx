fe165e6755ea7f60434a807a09aeb2d76c05a7e3
Irene Riportella <riportellairene@gmail.com>
2025-05-26 23:11:27 +0200

    feat:✨ update Logo component

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Logo() {
+  return (
+    <div className='flex'>
+      <img
+        src='src/assets/logo.svg'
+        className='size-11'
+      />
+      <p className='fredoka-lunara self-center text-2xl pl-2'>lunara</p>
+    </div>
+  );
+}
+
+export default Logo;
