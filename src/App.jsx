ed564f89ba2a713259c3d4acf2f38fcabc192e83
Irene Riportella <riportellairene@gmail.com>
2025-05-26 23:13:24 +0200

    feat:✨ display Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React from 'react';
+import Header from '../src/components/Header';
 
 function App() {
   return (
     <>
-      <h1>HELLO LUNARA!!</h1>
+      <Header />
     </>
   );
 }
