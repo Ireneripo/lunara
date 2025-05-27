cb7c3a467ef57fcc20c9987db401124e405b6d79
Irene Riportella <riportellairene@gmail.com>
2025-05-27 11:45:03 +0200

    feat:✨ add arrow to SuperHeader child

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
   return (
     <>
       {/* <div className="flex items-center mt-7"> */}
-      <SuperHeader>Lunara therapy is now available</SuperHeader>
+      <SuperHeader>Lunara therapy is now available →</SuperHeader>
       <div className='flex justify-between mx-12 mt-4'>
         <Navbar />
         <Options />
