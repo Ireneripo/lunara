a20666f28a02f9ef3e46125cea2471d016609022
Irene Riportella <riportellairene@gmail.com>
2025-05-27 23:53:10 +0200

    style:💄 improve Hero style

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,13 +3,14 @@ import Button from '../Button/Button';
 
 function Hero() {
   return (
-    <div className='flex justify-between mx-44 gap-36 mt-20'>
+    <div className='flex justify-between mx-40 gap-36 mt-36'>
       <div>
-        <h1 className='text-6xl font-bold mb-4'>
+        <h1 className='text-6xl font-semibold mb-4'>
           Find your path to mental wellness
         </h1>
-        <p className='text-lg'>
-          Discover the power of mindfulness and meditation with our app.
+        <p className='text-lg text-gray-500'>
+          Discover the power of mindfulness and meditation with our app, improve
+          your sleep and say goodbay to stressful days.
         </p>
         <Button className='bg-[#141313]/90 hover:bg-[#141313] text-white px-11 py-5 rounded-full shadow shadow-gray-500 text-xl font-semibold mt-6 cursor-pointer'>
           Try 14 days free
