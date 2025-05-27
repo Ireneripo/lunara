577f3dc98f4bd3b8e5cb80e9579e39ecbf97d2c4
Irene Riportella <riportellairene@gmail.com>
2025-05-27 10:45:12 +0200

    feat:✨ create SuperHeader component

---
+++ src/components/SuperHeader/SuperHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SuperHeader({ children }) {
+  return (
+    <div className='flex bg-[#FECE00] h-14.5 items-center justify-center text-lg underline'>
+      {children}
+    </div>
+  );
+}
+
+export default SuperHeader;
