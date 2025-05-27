beeffdb38084fb1e640c4f453b8f3cf733277e3b
Irene Riportella <riportellairene@gmail.com>
2025-05-27 11:44:24 +0200

    style:💄 add pointer to SuperHeader and underline only when hovering

--- src/components/SuperHeader/SuperHeader.jsx
+++ src/components/SuperHeader/SuperHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function SuperHeader({ children }) {
   return (
-    <div className='flex bg-[#FECE00] h-14.5 items-center justify-center text-lg underline'>
+    <div className='flex bg-[#FECE00] h-14.5 items-center justify-center text-lg hover:underline cursor-pointer'>
       {children}
     </div>
   );
