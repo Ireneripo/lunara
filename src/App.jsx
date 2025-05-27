8f00a5bbcaffd80063d14855234b9601a7c72ff5
Irene Riportella <riportellairene@gmail.com>
2025-05-27 15:55:09 +0200

    feat:✨ use Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../src/components/Header';
+import Hero from '../src/components/Hero';
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
