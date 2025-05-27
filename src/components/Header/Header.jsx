0f53ca3085edd1aed31853696361fd689f983968
Irene Riportella <riportellairene@gmail.com>
2025-05-27 10:45:33 +0200

    feat:✨ use SuperHeader component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import Options from '../Options/Options';
+import SuperHeader from '../SuperHeader/SuperHeader';
 
 function Header() {
   return (
     <>
       {/* <div className="flex items-center mt-7"> */}
-      <div className='flex justify-between mx-12 mt-7'>
+      <SuperHeader>Lunara therapy is now available</SuperHeader>
+      <div className='flex justify-between mx-12 mt-4'>
         <Navbar />
         <Options />
       </div>
