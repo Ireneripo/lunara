9cea7cdf92f133ed2ef6ded26660b658b958d9ea
Irene Riportella <riportellairene@gmail.com>
2025-05-28 14:33:33 +0200

    style:💄 remove hovering from 'div' and add a 'p' element

--- src/components/SuperHeader/SuperHeader.jsx
+++ src/components/SuperHeader/SuperHeader.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 function SuperHeader({ children }) {
   return (
-    <div className='flex bg-[#FECE00] h-14.5 items-center justify-center text-lg hover:underline cursor-pointer'>
-      {children}
+    <div className='flex bg-[#FECE00] h-14.5 items-center justify-center'>
+      <p className='text-[#141313] text-lg cursor-pointer hover:underline'>
+        {children}
+      </p>
     </div>
   );
 }
