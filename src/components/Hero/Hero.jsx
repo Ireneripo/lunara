72dd53f77f28532c79031e8a2e96f23e5d5201fc
Irene Riportella <riportellairene@gmail.com>
2025-05-29 21:22:04 +0200

    feat:✨ add background image to Hero and improve layout

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,25 +3,32 @@ import Button from '../Button/Button';
 
 function Hero() {
   return (
-    <div className='flex justify-between mx-40 gap-36 mt-36'>
-      <div>
-        <h1 className='text-6xl font-semibold mb-4'>
-          Find your path to mental wellness
-        </h1>
-        <p className='text-lg text-gray-500'>
-          Discover the power of mindfulness and meditation with our app, improve
-          your sleep and say goodbye to stressful days.
-        </p>
-        <Button className='bg-[#141313]/90 hover:bg-[#141313] text-white px-11 py-5 rounded-full shadow shadow-gray-500 text-xl font-semibold mt-6 cursor-pointer'>
-          Try 14 days free
-        </Button>
-      </div>
-      <div className='w-1/2 flex'>
-        <img
-          className='w-full self-center'
-          src='src/assets/hero-phone.png'
-          alt=''
-        />
+    <div className='w-full flex justify-center'>
+      <div className='flex gap-12 p-12 my-0 w-[85%]'>
+        <div className='self-center'>
+          <h1 className='text-6xl font-semibold mb-4'>
+            Find your path to mental wellness
+          </h1>
+          <p className='text-lg text-gray-500'>
+            Discover the power of mindfulness and meditation with our app,
+            improve your sleep and say goodbye to stressful days.
+          </p>
+          <Button className='bg-[#141313]/90 hover:bg-[#141313] text-white px-11 py-5 rounded-full shadow shadow-gray-500 text-xl font-semibold mt-6 cursor-pointer'>
+            Try for $0
+          </Button>
+        </div>
+        <div className='relative w-[850px] h-[500px]'>
+          <img
+            className='absolute w-[850px]'
+            src='src/assets/phone-background.png'
+            alt='Phone background'
+          />
+          <img
+            className='absolute z-10 w-[350px] left-[100px]'
+            src='src/assets/hero-phone.png'
+            alt='Phone mockup'
+          />
+        </div>
       </div>
     </div>
   );
