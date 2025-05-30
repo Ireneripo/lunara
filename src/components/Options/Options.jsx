c58533eb8b5a43f804477259ec4583632d84d27a
Irene Riportella <riportellairene@gmail.com>
2025-05-30 17:37:13 +0200

    style:💄 add min-width to Button

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -16,7 +16,7 @@ function Options() {
       >
         Help
       </a>
-      <Button className='bg-blue-600 hover:bg-blue-700 text-white rounded-full px-6 py-3 shadow shadow-gray-500'>
+      <Button className='bg-blue-600 hover:bg-blue-700 text-white rounded-full px-6 py-3 shadow shadow-gray-500 min-w-fit'>
         Try for free
       </Button>
     </div>
