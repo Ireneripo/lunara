c7f5497c6060125dae8061b3b79ef7775cfe6d21
Irene Riportella <riportellairene@gmail.com>
2025-05-31 23:12:36 +0200

    feat:✨ use Need component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../src/components/Header';
 import Hero from '../src/components/Hero';
+import Need from '../src/components/Need';
 
 function App() {
   return (
     <div className='bg-[#F9F4F2]'>
       <Header />
       <Hero />
+      <Need />
     </div>
   );
 }
