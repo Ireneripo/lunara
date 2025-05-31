df87cce68f3534a7920bd8c478f92a5372eb7657
Irene Riportella <riportellairene@gmail.com>
2025-05-31 23:14:40 +0200

    style:💄 add padding to Hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import Button from '../Button/Button';
 
 function Hero() {
   return (
-    <div className='w-full flex justify-center'>
+    <div className='w-full flex justify-center py-8'>
       <div className='flex gap-20 px-14 w-[85%]'>
         <div className='self-center'>
           <h1 className='text-6xl font-semibold mb-4'>
