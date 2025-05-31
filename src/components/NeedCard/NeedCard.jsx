f99076f538a95e087ceeff93db8d17cbd7ea3cca
Irene Riportella <riportellairene@gmail.com>
2025-05-31 23:11:34 +0200

    feat:✨ add content to NeedCard

--- src/components/NeedCard/NeedCard.jsx
+++ src/components/NeedCard/NeedCard.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
-function NeedCard() {
-  return <div className="flex justify-center items-center">
-    <p>Practice meditation</p>
-    <div className="flex">
-      <img src='src/assets/need.png' alt='need' />
-      <i></i>
+function NeedCard({imageSrc}) {
+  return (
+    <div className="flex items-center gap-20 border-2 border-gray-300 rounded-lg py-2 pl-6 pr-4 cursor-pointer">
+      <p className="text-2xl">Practice meditation</p>
+      <div className="flex items-center">
+        <img
+          src={imageSrc}
+          className="size-16"
+          alt=""
+        />
+        <img
+          src="src/assets/Chevron Right Icon.svg"
+          className="size-8"
+          alt=""
+        />
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default NeedCard;
