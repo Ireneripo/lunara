10d5c81716f94b7054cf6112e4c9cdb43ac1cde0
Irene Riportella <riportellairene@gmail.com>
2025-05-31 23:12:15 +0200

    feat:✨ include cards in Need component

--- src/components/Need/Need.jsx
+++ src/components/Need/Need.jsx
@@ -3,9 +3,18 @@ import NeedCard from '../NeedCard/NeedCard';
 
 function Need() {
   return (
-    <div className=" flex flex-col">
-      <h2>What kind of meditation do you need right now?</h2>
-      <NeedCard />
+    <div className="flex flex-col items-center pt-16 pb-32 px-12 bg-white">
+      <h2 className="text-4xl font-semibold">
+        What kind of meditation do you need right now?
+      </h2>
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-6 mt-12">
+        <NeedCard imageSrc={'src/assets/need7.svg'}/>
+        <NeedCard imageSrc={'src/assets/need2.svg'}/>
+        <NeedCard imageSrc={'src/assets/need3.svg'}/>
+        <NeedCard imageSrc={'src/assets/need4.svg'}/>
+        <NeedCard imageSrc={'src/assets/need8.svg'}/>
+        <NeedCard imageSrc={'src/assets/need6.svg'}/>
+      </div>
     </div>
   );
 }
