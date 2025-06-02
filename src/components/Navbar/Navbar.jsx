5b7b862cae81b39ecfd38a93c852cd7fafc5e72b
Irene Riportella <riportellairene@gmail.com>
2025-06-02 23:08:21 +0200

    style:💄 add pointer to Navbar elements

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,15 +5,15 @@ import Logo from '../Logo/Logo';
 function Navbar() {
   return (
     <>
-      <div className='flex items-center'>
+      <div className="flex items-center">
         <Logo />
-        <ul className='flex gap-6 ml-20'>
-          <li className='hover:font-medium min-w-fit'>For You</li>
-          <li className='hover:font-medium min-w-fit'>For Business</li>
-          <li className='hover:font-medium min-w-fit'>For Providers</li>
-          <li className='hover:font-medium min-w-fit'>Our Plans</li>
-          <li className='hover:font-medium min-w-fit'>Resources</li>
-          <li className='hover:font-medium min-w-fit'>About</li>
+        <ul className="flex gap-6 ml-20 cursor-pointer">
+          <li className="hover:font-medium min-w-fit">For You</li>
+          <li className="hover:font-medium min-w-fit">For Business</li>
+          <li className="hover:font-medium min-w-fit">For Providers</li>
+          <li className="hover:font-medium min-w-fit">Our Plans</li>
+          <li className="hover:font-medium min-w-fit">Resources</li>
+          <li className="hover:font-medium min-w-fit">About</li>
         </ul>
       </div>
 
