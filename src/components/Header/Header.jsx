012a803fd6fa1c61f9f292a8c893d8667a6710fd
Irene Riportella <riportellairene@gmail.com>
2025-06-02 11:04:01 +0200

    feat:✨ add shadow to Header when scrolling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,23 @@ import Options from '../Options/Options';
 import SuperHeader from '../SuperHeader/SuperHeader';
 
 function Header() {
+  const [isScrolled, setIsScrolled] = React.useState(false);
+
+  React.useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <>
       <SuperHeader>Lunara therapy is now available →</SuperHeader>
-      <div className='bg-[#ffffff] sticky top-0'>
+      <div
+        className={`bg-[#ffffff] sticky top-0 ${isScrolled ? 'shadow-lg' : ''}`}
+      >
         <div className='flex justify-between mx-12 h-20'>
           <Navbar />
           <Options />
