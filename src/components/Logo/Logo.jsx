4d5d2651b6d36bcc9ccbb98d92136c62f718322a
Irene Riportella <riportellairene@gmail.com>
2025-06-03 14:58:56 +0200

    feat:✨ manage responsive logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 
 function Logo() {
-  return (
-    <div className='flex pb-1.5'>
+  const [isDesktop, setIsDesktop] = React.useState(true);
+
+  React.useEffect(() => {
+    const checkScreenSize = () => {
+      setIsDesktop(window.innerWidth >= 1440);
+    };
+
+    checkScreenSize();
+
+    window.addEventListener('resize', checkScreenSize);
+    return () => window.removeEventListener('resize', checkScreenSize);
+  }, []);
+
+  return isDesktop ? (
+    <div className="flex pb-1.5">
       <img
-        src='src/assets/logo.svg'
-        className='size-10'
+        src="src/assets/logo.svg"
+        className="size-10"
       />
-      <p className='fredoka-lunara self-center text-2xl pl-1.5'>lunara</p>
+      <p className="fredoka-lunara self-center text-2xl pl-1.5">lunara</p>
     </div>
+  ) : (
+    <img
+      src="src/assets/logo.svg"
+      className="size-10"
+    />
   );
 }
 
