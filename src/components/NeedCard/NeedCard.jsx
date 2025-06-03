3f2c5545f42dc472a94ac9dc7f6328defe9f7885
Irene Riportella <riportellairene@gmail.com>
2025-06-03 21:47:50 +0200

    feat:✨ use Phosphor icon and add animation

--- src/components/NeedCard/NeedCard.jsx
+++ src/components/NeedCard/NeedCard.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { CaretRight } from '@phosphor-icons/react';
 
 function NeedCard({ imageSrc, title }) {
   return (
-    <div className="flex items-center w-full border-2 border-gray-300 rounded-lg p-3 cursor-pointer overflow-hidden lg:h-20">
+    <div className="group flex items-center w-full border-2 border-gray-300 rounded-lg p-3 cursor-pointer overflow-hidden lg:h-20">
       <p className="text-lg min-[1440px]:text-2xl truncate flex-1 font-light">
         {title}
       </p>
@@ -15,10 +16,10 @@ function NeedCard({ imageSrc, title }) {
           />
         </div>
         <div className="w-6 h-6 lg:w-8 lg:h-8 flex-shrink-0">
-          <img
-            src="src/assets/Chevron Right Icon.svg"
-            className="w-full h-full object-contain"
-            alt="Chevron Right Icon"
+          <CaretRight
+            size={20}
+            weight="bold"
+            className="transition-transform duration-200 ease-in-out group-hover:translate-x-2"
           />
         </div>
       </div>
