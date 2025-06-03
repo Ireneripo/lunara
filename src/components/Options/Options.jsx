4d2bb8ffba0ddaba4f84dc0c3e0a7df46de65c5e
Irene Riportella <riportellairene@gmail.com>
2025-06-03 21:55:05 +0200

    refactor:♻️ run prettier

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -3,20 +3,20 @@ import Button from '../Button/Button';
 
 function Options() {
   return (
-    <div className='flex items-center gap-16'>
+    <div className="flex items-center gap-16">
       <a
-        href='#'
-        className='hover:font-medium'
+        href="#"
+        className="hover:font-medium"
       >
         Login
       </a>
       <a
-        href='#'
-        className='hover:font-medium'
+        href="#"
+        className="hover:font-medium"
       >
         Help
       </a>
-      <Button className='bg-blue-600 hover:bg-blue-700 text-white rounded-full px-6 py-3 shadow shadow-gray-500 min-w-fit'>
+      <Button className="bg-blue-600 hover:bg-blue-700 text-white rounded-full px-6 py-3.5 shadow shadow-gray-500 min-w-fit font-semibold text-sm cursor-pointer">
         Try for free
       </Button>
     </div>
