af2102898c2e4be22375f5b15ff24140ec6ea650
Irene Riportella <riportellairene@gmail.com>
2025-06-03 11:10:33 +0200

    style:💄 add max-width for larger screnns and fix margins

--- src/components/Need/Need.jsx
+++ src/components/Need/Need.jsx
@@ -3,11 +3,11 @@ import NeedCard from '../NeedCard/NeedCard';
 
 function Need() {
   return (
-    <div className="flex flex-col items-center pt-16 pb-32 px-12 bg-white">
-      <h2 className="text-4xl font-semibold">
+    <div className="flex flex-col items-center pt-16 pb-32 px-12 bg-white min-[1440px]:max-w-[90rem] mx-auto">
+      <h2 className="text-4xl font-semibold mt-5 mb-10">
         What kind of meditation do you need right now?
       </h2>
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-6 mt-12 w-[98%]">
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-6 w-[98%]">
         <NeedCard
           imageSrc={'src/assets/need7.svg'}
           title="Stress less"
