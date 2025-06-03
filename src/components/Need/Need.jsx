c4fe56b82591d7131005b25963cc4f805ba8c2b4
Irene Riportella <riportellairene@gmail.com>
2025-06-03 11:52:00 +0200

    style:💄 shrink h2 on Need component

--- src/components/Need/Need.jsx
+++ src/components/Need/Need.jsx
@@ -4,7 +4,7 @@ import NeedCard from '../NeedCard/NeedCard';
 function Need() {
   return (
     <div className="flex flex-col items-center pt-16 pb-32 px-12 bg-white min-[1440px]:max-w-[90rem] mx-auto">
-      <h2 className="text-4xl font-semibold mt-5 mb-10">
+      <h2 className="text-3xl font-semibold mt-5 mb-10">
         What kind of meditation do you need right now?
       </h2>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-6 w-[98%]">
