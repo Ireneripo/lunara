5864457d42c810a9097a8ddeb25b32ed95970639
Irene Riportella <riportellairene@gmail.com>
2025-06-06 15:10:19 +0200

    feat:✨ create CarouselContainer component

---
+++ src/components/CarouselContainer/CarouselContainer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function CarouselContainer() {
+  return (
+    <div className="pt-8 px-6 pb-6">
+      <h2 className="text-[2.5rem] font-semibold mt-5 mb-10 text-center">
+        The everyday mental health app you where looking for
+      </h2>
+    </div>
+  );
+}
+
+export default CarouselContainer;
