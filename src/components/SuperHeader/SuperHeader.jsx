daeaa610cb30083e723bb3145531c06c039ddb23
Irene Riportella <riportellairene@gmail.com>
2025-06-08 18:03:57 +0200

    style:💄 set underline by default

--- src/components/SuperHeader/SuperHeader.jsx
+++ src/components/SuperHeader/SuperHeader.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function SuperHeader({ children }) {
   return (
-    <div className='flex bg-[#FECE00] h-14.5 items-center justify-center'>
-      <p className='text-[#141313] text-lg cursor-pointer hover:underline'>
+    <div className="flex bg-[#FECE00] h-14.5 items-center justify-center">
+      <p className="text-[#141313] text-lg cursor-pointer underline">
         {children}
       </p>
     </div>
